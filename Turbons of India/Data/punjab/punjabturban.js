c3adfb8af791af203fea04cc550002ed4b36cd51
sanjai-001 <134257581+sanjai-001@users.noreply.github.com>
2024-10-01 22:39:23 +0530

    Add files via upload

---
+++ Turbons of India/Data/punjab/punjabturban.js
@@ -0,0 +1,97 @@
+let outputWidth;
+let outputHeight;
+
+let faceTracker; // Face Tracking
+let videoInput;
+
+let imgSpidermanMask; // Spiderman Mask Filter
+let imgDogEarRight, imgDogEarLeft, imgDogNose; // Dog Face Filter
+
+let selected = 0; // Default no filter
+
+/*
+ * **p5.js** library automatically executes the `preload()` function. Basically, it is used to load external files. In our case, we'll use it to load the images for our filters and assign them to separate variables for later use.
+*/
+function preload()
+{
+  // Spiderman Mask Filter asset
+  imgturban = loadImage("https://i.ibb.co/tzfnVVM/PUNJAB-1.png");
+}
+
+/**
+ * In p5.js, `setup()` function is executed at the beginning of our program, but after the `preload()` function.
+*/
+function setup()
+{
+  const maxWidth = Math.min(windowWidth, windowHeight);
+  pixelDensity(1);
+  outputWidth = maxWidth;
+  outputHeight = maxWidth * 0.75; // 4:3
+
+  createCanvas(outputWidth, outputHeight);
+
+  // webcam capture
+  videoInput = createCapture(VIDEO);
+  videoInput.size(outputWidth, outputHeight);
+  videoInput.hide();
+
+  // select filter
+  const sel = createSelect();
+  const selectList = ['Turban',]; // list of filters
+  sel.option('Video Feed', -1); // Default no filter
+  for (let i = 0; i < selectList.length; i++)
+  {
+    sel.option(selectList[i], i);
+  }
+  sel.changed(applyFilter);
+
+  // tracker
+  faceTracker = new clm.tracker();
+  faceTracker.init();
+  faceTracker.start(videoInput.elt);
+}
+
+// callback function
+function applyFilter()
+{
+  selected = this.selected(); // change filter type
+}
+
+/*
+ * In p5.js, draw() function is executed after setup(). This function runs inside a loop until the program is stopped.
+*/
+function draw()
+{
+  image(videoInput, 0, 0, outputWidth, outputHeight); // render video from webcam
+
+  // apply filter based on choice
+  switch(selected)
+  {
+    case '-1': break;
+    case '0': drawturban(); break;
+  }
+}
+
+// Spiderman Mask Filter
+function drawturban()
+{
+  const positions = faceTracker.getCurrentPosition();
+  if (positions !== false)
+  {
+    push();
+    const wx = Math.abs(positions[13][0] - positions[1][0]) * 2.0; // The width is given by the face width, based on the geometry
+    const wy = Math.abs(positions[7][1] - Math.min(positions[16][1], positions[20][1])) * 1.6; // The height is given by the distance from nose to chin, times 2
+    translate(-wx/2,-wy/1.2);
+    image(imgturban, positions[33][0], positions[33][1] , wx, wy); // Show the mask at the center of the face
+    pop();
+  }
+}
+
+function windowResized()
+{
+  const maxWidth = Math.min(windowWidth, windowHeight);
+  pixelDensity(1);
+  outputWidth = maxWidth;
+  outputHeight = maxWidth * 0.75; // 4:3
+  resizeCanvas(outputWidth, outputHeight);
+}
